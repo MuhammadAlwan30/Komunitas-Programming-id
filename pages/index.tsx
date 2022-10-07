d49d0ff11d35752260d123e8994f0202e5ac4ee1
Muhammad Alwan <79246294+MuhammadAlwan30@users.noreply.github.com>
2022-10-08 02:05:54 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Index(){
     return (
  <>
-  <body className=flex bg-sky-500">
+  <body className="flex bg-sky-500">
   <header>
   <h1 className="font-sans bg-slate-500 text-center item-center">Selamat datang di komunitas programming indonesia </h1>
 </header>
