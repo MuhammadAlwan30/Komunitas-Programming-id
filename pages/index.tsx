5c953caac902a864b4d8f354ff9ea8cb095def80
Muhammad Alwan <79246294+MuhammadAlwan30@users.noreply.github.com>
2022-10-07 15:54:01 +0700

    add new files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 export default function Index(){
     return (
  <>
+  <body className=flex bg-sky-500">
   <header>
-  <h1 className="font-sans lightblue">Selamat datang </h1>
+  <h1 className="font-sans bg-slate-500 text-center item-center">Selamat datang di komunitas programming indonesia </h1>
 </header>
+</body>
  </>
     )
 }
